62bf83dfdbf40999c0a47d7f4285bc5755750f8a
Robbie <Robbie@email.com>
2025-11-08 11:17:20 +0900

    feat: 응답, 예외설정
    feat: 응답, 예외설정

---
+++ src/main/java/com/sparta/project/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.sparta.project.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ProductRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+
+}
